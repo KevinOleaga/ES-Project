d6a7e0bf442a392a001005f170ebe35204362dab
Diegomorales23 <31495100+Diegomorales23@users.noreply.github.com>
2017-10-31 21:34:00 -0600

    capas faltantes

---
+++ ES.DATA/Roles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
+
+namespace ES.DATA
+{
+    public class Roles
+    {
+        [AutoIncrement]
+        public int IdRol { get; set; }
+
+        public string Rol { get; set; }
+
+    }
+}
---
+++ ES.DATA/Usuarios.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.DataAnnotations;
+
+namespace ES.DATA
+{
+    public class Usuarios
+    {
+        public string Username { get; set; }
+
+        public int Password { get; set; }
+        [AutoIncrement]
+        public int IdRol { get; set; }
+
+        public int IntentosFallidos { get; set; }
+
+        public bool CuentasBloqueada { get; set; }
+
+        public byte[] Foto { get; set; }
+
+        public int? Telefono { get; set; }
+
+    }
+
+}
