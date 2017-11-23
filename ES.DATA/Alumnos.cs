0064556fc5c251d8a939de1b93e6eb09de122795
Kevin Oleaga <oleaga@outlook.com>
2017-11-22 20:39:55 -0600

    new update 22/11/217

--- ES.DATA/Alumnos.cs
+++ ES.DATA/Alumnos.cs
@@ -9,20 +9,18 @@ namespace ES.DATA
 {
     public class Alumnos
     {
-        [AutoIncrement]
-        public int IdAlumno { get; set; }
-        public string Nombre { get; set; }
         public string Cedula { get; set; }
+        public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string Telefono { get; set; }
         public string Email { get; set; }
         public string Padecimientos { get; set; }
-        public string Direcion { get; set; }
-        public int IdEncargado { get; set; }
+        public string Direccion { get; set; }
+        public string IdEncargado { get; set; }
         public bool Graduado { get; set; }
         public bool Abandono { get; set; }
         public string AñoGraduacion { get; set; }
-
-
+        public string Sección { get; set; }
+        public string IdProfeGuia { get; set; }
     }
 }
--- ES.DATA/Grupo_Profesor.cs
+++ ES.DATA/Grupo_Profesor.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ServiceStack.DataAnnotations;
-
-namespace ES.DATA
+﻿namespace ES.DATA
 {
     public class Grupo_Profesor
-{
-        
+    {
         public int IdGrupo { get; set; }
 
-    public int IdProfesor { get; set; }
-
-}
-
+        public int IdProfesor { get; set; }
+    }
 }
